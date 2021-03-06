4d3fdaa57f2b000537fab795f55fe2777df03523
Аймкулова Ботагоз <botagoz.aimkulova@gmail.com>
2021-03-06 11:59:46 +0600

    #1 Создано приложение, добавлены кнопки и стили к ним

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.display}>0</Text>
+      <View style={styles.wrapper}>
+        <View style={styles.wrapperSymbol}>
+          <TouchableOpacity style={styles.number}>
+            <Text style={styles.text}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.number}>
+            <Text style={styles.text}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.number}>
+            <Text style={styles.text}>3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.symbol}>
+            <Text style={styles.text}>+</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.wrapperSymbol}>
+          <TouchableOpacity style={styles.number}>
+            <Text style={styles.text}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.number}>
+            <Text style={styles.text}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.number}>
+            <Text style={styles.text}>6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.symbol}>
+            <Text style={styles.text}>-</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.wrapperSymbol}>
+          <TouchableOpacity style={styles.number}>
+            <Text style={styles.text}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.number}>
+            <Text style={styles.text}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.number}>
+            <Text style={styles.text}>9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.symbol}>
+            <Text style={styles.text}>*</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.wrapperSymbol}>
+          <TouchableOpacity style={styles.number}>
+            <Text style={styles.text}>•</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.numberNull}>
+            <Text style={styles.text}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.symbol}>
+            <Text style={styles.text}>/</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.wrapperSymbol}>
+          <TouchableOpacity style={styles.symbolEqual}>
+            <Text style={styles.textSymbol}>&lt;</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.symbolEqual}>
+            <Text style={styles.textSymbol}>=</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,75 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingVertical: 2,
+    paddingHorizontal: 2,
+    flexWrap: "wrap",
+  },
+  display: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+    paddingHorizontal: 2,
+    paddingTop: 10,
+    marginTop: 50,
+    fontSize: 40,
+    textAlign: "right",
+  },
+  wrapper: {
+    width: "100%",
+    flex: 8,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  wrapperSymbol: {
+    width: "100%",
+    flexDirection: "row",
+  },
+  number: {
+    width: "24.5%",
+    height: 120,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#89CFF0",
+    marginHorizontal: 1,
+    marginVertical: 1,
+  },
+  symbol: {
+    width: "24%",
+    height: 120,
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 1,
+    marginVertical: 1,
+    backgroundColor: "#A4C638",
+  },
+  numberNull: {
+    width: "49.6%",
+    height: 120,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#89CFF0",
+    marginHorizontal: 1,
+    marginVertical: 1,
+  },
+  symbolEqual: {
+    width: "49.4%",
+    height: 90,
+    backgroundColor: "#FFA500",
+    marginHorizontal: 1,
+    marginVertical: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 26,
+  },
+  textSymbol: {
+    fontSize: 40,
+    color: "#fff",
+  },
+});
